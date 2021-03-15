a1e63721bc52f6037b936e8b737d76ab3f7c22dc
Valentino Aguiar <w1682146@my.westminster.ac.uk>
2021-03-15 16:19:04 +0000

    Add times child + parent finish, pyobject created

--- code.c
+++ code.c
@@ -8,13 +8,21 @@
 
 //Global variable declaration
 int fd[2], int_caught, tstp_caught, child_complete, saved_stdout;
+
+struct tm st;
+struct tm ct;
+struct tm pt;
+
 void child_sig_handler(int signo);
 void parent_sig_handler(int signo);
 void create_status_file(int pid);
-void store_to_database(char **numArray);
+void store_to_database(char numbers[]);
 void generateNumber();
 
 int main() {
+        // Start time of program is stored
+        time_t T= time(NULL);
+        st = *localtime(&T);
         int pid;
         //Ensuring the pipe is created successfully
         if (pipe(fd) < 0)
@@ -32,29 +40,21 @@ int main() {
 
         //Parent code
         else if (pid > 0){
-                saved_stdout = dup(1); //save STDOUT to temp FD
-                create_status_file(pid);
-                dup2(saved_stdout, 1); //reset STDOUT to regular output
-                close(saved_stdout);
                 close(fd[1]); //close write end of pipe
-                dup2(fd[0], fileno(stdin)); //read end of pipe bound to stdin
+                dup2(fd[0], 0); //read end of pipe bound to stdin
                 
                 while(child_complete != 1){} //halts parent whilst child not complete
-
-                //Variables for reading line by line
-                char *line;
-                size_t bufsize = 50;
-                int i=0;
-                char **numArray;
-                numArray = malloc(bufsize * sizeof(char*)); //allocates required memory to array pointer
                 
-                //While line isn't empty, copy the number into array
-                while ((getline(&line, &bufsize, stdin)) != -1) {
-                        numArray[i] = malloc(strlen(line) + 1); //allocates extra memory for new line
-                        strcpy(numArray[i], line); //copies string into array memory location
-                        i++;
-                }
-                store_to_database(numArray);
+                // produce and store child finish time
+                time_t childTime= time(NULL);
+                ct = *localtime(&childTime);
+                
+                // Read numbers into string
+                char numbers[200];
+                scanf("%s",numbers);
+
+                store_to_database(numbers);
+                create_status_file(pid);
                 close(fd[0]); //close read end of pipe
         }
 
@@ -91,37 +91,48 @@ void parent_sig_handler(int signo){
 
 void create_status_file(int pid){
         char *file = "status.txt";
-        
-        //Creating structure of current system time
-        time_t T= time(NULL);
-        struct tm tm = *localtime(&T);
-
         pid_t parentPID = getpid(); // Gets parent PID
         
         int statusDescriptor = open(file, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR); //status file opened for write as file descriptor fd
-        dup2(statusDescriptor, fileno(stdout)); //stdout file descriptor contents are duped to status file desciptor
+        dup2(statusDescriptor, 1); //stdout file descriptor contents are duped to status file desciptor
        
         // Make use of stat command to get information about inode
         struct stat file_stat;
         int inode;
         fstat (statusDescriptor, &file_stat);
 
-        printf("Program ran at: %02d:%02d:%0.2d\n",tm.tm_hour,tm.tm_min,tm.tm_sec); //time is printed to file
+        time_t parentTime= time(NULL);
+        pt = *localtime(&parentTime);
+
+        printf("Program ran at: %02d:%02d:%0.2d\n",st.tm_hour,st.tm_min,st.tm_sec); //time is printed to file
+        printf("Child terminated at: %02d:%02d:%0.2d\n",ct.tm_hour,ct.tm_min,ct.tm_sec); //time is printed to file
+        printf("Parent terminated at: %d:%d:%d\n", pt.tm_hour,pt.tm_min,pt.tm_sec); //time is printed to file
         printf("Child PID is %d, the parent PID is %d\n", pid, parentPID);
         printf("Inode Number of file: %d, User ID of owner: %d, Group ID: %d, Date Created (unix timestamp): %d", file_stat.st_ino, file_stat.st_uid, file_stat.st_gid, file_stat.st_ctim);
+        
         fflush(stdout);
         fsync(statusDescriptor);
         close(statusDescriptor);
 }
 
 void generateNumber(){
-        srand(time(0));
+
+        //seed srandom with gettimeofday(), produces more random numbers than time()
+        struct timeval tm;
+        gettimeofday(&tm, NULL);
+        srandom(tm.tv_sec + tm.tv_usec * 1000000ul);
+
         int num = (rand() % (50 - 10 + 1)) + 10;
-        printf("%d\n", num);
+        printf("%d,", num);
 }
 
-void store_to_database(char **numArray){
+void store_to_database(char numbers[]){
         Py_Initialize();
-        PyRun_SimpleString("print('hello world')");
+
+
+        PyObject* numberStr = Py_BuildValue("c", numbers);
+
+
+
         Py_Finalize();
 }
